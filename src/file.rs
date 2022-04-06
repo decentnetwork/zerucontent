6fc55695b6f3d78e7c8086e5017864be0221b295
canewsin <canews.in@gmail.com>
2022-04-06 20:50:41 +0530

    Custom Debug Impl for File

--- src/file.rs
+++ src/file.rs
@@ -1,9 +1,20 @@
 use serde::{Deserialize, Serialize};
 use std::cmp::PartialEq;
 use std::default::Default;
+use std::fmt::Debug;
 
-#[derive(Serialize, Deserialize, Default, Debug, PartialEq, Clone)]
+#[derive(Serialize, Deserialize, Default, PartialEq, Clone)]
 pub struct File {
     pub sha512: String,
     pub size: usize,
+}
+
+impl Debug for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "File Stats: sha512: {}, size: {}",
+            self.sha512, self.size
+        )
+    }
 }
