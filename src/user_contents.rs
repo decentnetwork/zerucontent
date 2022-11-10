a1a5b82701119e90ad02818b0448583776aedb61
canewsin <canews.in@gmail.com>
2022-11-10 07:28:04 +0530

    user_contents::UserContents - derive serde(default) instead of wrapping in Option - Add PermissionRulesType for bool type rules with serde(untagged)

--- src/user_contents.rs
+++ src/user_contents.rs
@@ -4,23 +4,38 @@ use std::collections::BTreeMap;
 use std::default::Default;
 
 #[derive(Serialize, Deserialize, Default, PartialEq, Clone)]
+#[serde(default)]
 pub struct UserContents {
     pub archived: BTreeMap<String, usize>,
     pub archived_before: usize,
     pub cert_signers: BTreeMap<String, Vec<String>>,
     pub cert_signers_pattern: String,
-    pub permission_rules: BTreeMap<String, Option<PermissionRules>>,
-    pub permissions: BTreeMap<String, Option<PermissionRules>>,
+    pub permission_rules: BTreeMap<String, PermissionRulesType>,
+    pub permissions: BTreeMap<String, PermissionRulesType>,
     pub content_inner_path: String,
-    pub optional: Option<String>,
+    pub optional: String,
     pub relative_path: String,
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+#[serde(untagged)]
+pub enum PermissionRulesType {
+    None(bool),
+    Rules(PermissionRules),
+}
+
+impl Default for PermissionRulesType {
+    fn default() -> Self {
+        PermissionRulesType::None(false)
+    }
+}
+
 #[derive(Serialize, Deserialize, Default, PartialEq, Clone)]
+#[serde(default)]
 pub struct PermissionRules {
-    files_allowed: Option<String>,
-    files_allowed_optional: Option<String>,
-    max_size: usize,
-    max_size_optional: usize,
-    signers: Vec<String>,
+    pub files_allowed: String,
+    pub files_allowed_optional: String,
+    pub max_size: usize,
+    pub max_size_optional: usize,
+    pub signers: Vec<String>,
 }
