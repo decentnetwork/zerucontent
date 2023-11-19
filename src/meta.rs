c06278831d05c24ac91c07a73d02a08764442b70
canewsin <canews.in@gmail.com>
2023-11-19 19:06:56 +0530

    Meta : custom Serialize implementation

--- src/meta.rs
+++ src/meta.rs
@@ -1,12 +1,33 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-use crate::util::is_default;
+use serde::ser::{Serialize, SerializeStruct};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+#[derive(Deserialize, Debug, Clone, Default)]
 pub struct Meta {
-    #[serde(skip_serializing_if = "is_default")]
     pub inner_path: String,
     // #[serde(skip_serializing_if = "is_default")]
     pub description: Option<String>,
     pub zeronet_version: Option<String>,
+}
+
+impl Serialize for Meta {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut s = serializer.serialize_struct(
+            "Meta",
+            false as usize
+                + 1
+                + self.description.is_some() as usize
+                + self.zeronet_version.is_some() as usize,
+        )?;
+        if let Some(ref description) = self.description {
+            s.serialize_field("description", description)?;
+        }
+        if let Some(ref zeronet_version) = self.zeronet_version {
+            s.serialize_field("zeronet_version", zeronet_version)?;
+        }
+        s.end()
+    }
 }
