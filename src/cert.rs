ec7254c7d29cd5ff3b1e32083cc87b47975dba8f
canewsin <canews.in@gmail.com>
2023-11-20 01:15:35 +0530

    Cert: derive PartialEq, Eq traits

--- src/cert.rs
+++ src/cert.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
 pub struct Cert {
     #[serde(rename = "cert_auth_type")]
     auth_type: String,
